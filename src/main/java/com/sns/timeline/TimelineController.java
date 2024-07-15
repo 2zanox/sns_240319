cc7f687ebf4304973001e1ad1323290ac708f691
DESKTOP-OA6AHKT\j9712 <j971229@gmail.com>
2024-07-16 03:49:51 +0900

    240716 타임라인 마크업

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TimelineController {
 	
+	// http://localhost:8080/timeline/timeline-view
 	@GetMapping("/timeline/timeline-view")
 	public String timelineView() {
 		return "timeline/timeline";
