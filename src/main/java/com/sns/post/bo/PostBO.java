96792cdb52323660b3a69695dfe2c19a16f39aa8
DESKTOP-QAGTK6B\user <test@test.com>
2024-07-16 18:50:56 +0900

    240716 타임라인 글 목록

---
+++ src/main/java/com/sns/post/bo/PostBO.java
@@ -0,0 +1,10 @@
+package com.sns.post.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostBO {
+	
+	
+	
+}
--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,14 +1,30 @@
 package com.sns.timeline;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sns.post.bo.PostBO;
 
 @Controller
 public class TimelineController {
 	
+	@Autowired
+	private PostBO postBO;
+	
 	// http://localhost:8080/timeline/timeline-view
 	@GetMapping("/timeline/timeline-view")
-	public String timelineView() {
+	public String timelineView(Model model) {
+		
+		// DB 조회
+		//List<Post> postList = postBO.;
+		
+		// model
+		
+		
 		return "timeline/timeline";
 	}
 	
