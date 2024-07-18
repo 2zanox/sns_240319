12d74e51274ab6c44124b506f2b43e0b21539b3a
DESKTOP-OA6AHKT\j9712 <j971229@gmail.com>
2024-07-19 04:42:36 +0900

    240719 sns 파일 업로드 수정

--- src/main/java/com/sns/post/bo/PostBO.java
+++ src/main/java/com/sns/post/bo/PostBO.java
@@ -4,20 +4,46 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.sns.common.FileManagerService;
 import com.sns.post.entity.PostEntity;
 import com.sns.post.repository.PostRepository;
 
 @Service
 public class PostBO {
-	
+
 	@Autowired
 	private PostRepository postRepository;
 	
+	@Autowired
+	private FileManagerService fileManagerService;
+
 	// input: X
 	// output: List<PostEntity>
 	public List<PostEntity> getPostEntityList() {
 		return postRepository.findByOrderByIdDesc();
 	}
+
+	// input: 4개의 파라미터
+	// output: PostEntity
+	public PostEntity addPost(int userId, String userLoginId, String content, MultipartFile file) {
+
+		// 업로드 후 imagePath를 받아옴
+		String imagePath = fileManagerService.uploadFile(file, userLoginId);
+		
+		return postRepository.save(
+				PostEntity.builder()
+				.userId(userId)
+				.content(content)
+				.imagePath(imagePath)
+				.build());
+	}
+	
+	// input: postId
+	// output Post or null
+//	public PostEntity getPostByPostId(int postId) {
+//		
+//	}
 
 }
